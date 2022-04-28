911c9a8e1fb7af982e45196044afee6957ec40c0
RenatoConfuorto <renato.confuorto@outlook.it>
2022-04-28 18:52:30 +0200

    Milestone 1

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const team = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
+
+//stampare i membri in console
+for(let i = 0; i < team.length; i++){
+  const currentMember = team[i];
+  console.log(`Membro n° ${i + 1}`);
+  console.log(`Nome: ${currentMember.name}`);
+  console.log(`Ruolo: ${currentMember.role}`);
+  console.log(`Immagine: ${currentMember.image}`);
+}
